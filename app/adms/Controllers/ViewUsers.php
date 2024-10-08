75549d743a6b7bbec5091d90b789d2780845553f
Luiz Felipe <luizfelipexilipe@gmail.com>
2024-10-08 16:20:57 -0300

    Tratando a URL

---
+++ app/adms/Controllers/ViewUsers.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Adms\Controllers;
+
+class ViewUsers
+{
+    public function indexListar(): void
+    {
+        echo "ver usuários <br>";
+    }
+}
--- core/ConfigController.php
+++ core/ConfigController.php
@@ -10,6 +10,9 @@ class ConfigController extends Config
     private string $urlMetodo;
     private string $urlParameter;
     private string $classLoad;
+    private array $format;
+    private string $urlSlugController;
+    private string $urlSlugMetodo;
 
     public function __construct()
     {
@@ -17,21 +20,21 @@ class ConfigController extends Config
 
         if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))) {
             $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
-
+            $this->clearUrl();
             $this->urlArray = explode('/', $this->url);
 
             // Define a Controller
             if(isset($this->urlArray[0])) {
-                $this->urlController = $this->urlArray[0];
+                $this->urlController = $this->slugController($this->urlArray[0]);
             } else {
-                $this->urlController = CONTROLLER;
+                $this->urlController = $this->slugController(CONTROLLER);
             }
 
             // Define o método
             if(isset($this->urlArray[1])) {
-                $this->urlMetodo = $this->urlArray[1];
+                $this->urlMetodo = $this->slugMetodo($this->urlArray[1]);
             } else {
-                $this->urlMetodo = METODO;
+                $this->urlMetodo = $this->slugMetodo(METODO);
             }
 
             // Define o parâmetro
@@ -41,14 +44,59 @@ class ConfigController extends Config
                 $this->urlParameter = '';
             }
         } else {
-            $this->urlController = CONTROLLER;
-            $this->urlMetodo = METODO;
+            $this->urlController = $this->slugController(CONTROLLER);
+            $this->urlMetodo = $this->slugMetodo(METODO);
             $this->urlParameter = '';
         }
 
         echo "Controller: {$this->urlController} <br>";
         echo "Método: {$this->urlMetodo} <br>";
         echo "Prâmetro: {$this->urlParameter} <hr> <br>";
+    }
+
+    private function clearUrl(): void
+    {
+        // Tira as tags (<a></a> <p></p>)
+        $this->url = strip_tags($this->url);
+
+        // Remove os espaços
+        $this->url = trim($this->url);
+
+        // Tira a / do final da url
+        $this->url = rtrim($this->url, '/');
+
+        // Eliminar caracteres
+        $this->format['a'] = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜüÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿRr"!@#$%&*()_-+={[}]?;:.,\\\'<>°ºª ';
+        $this->format['b'] = 'aaaaaaaceeeeiiiidnoooooouuuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr-------------------------------------------------------------------------------------------------';
+        $this->url = strtr(mb_convert_encoding($this->url, "ISO-8859-1", "UTF-8"), mb_convert_encoding($this->format["a"], "ISO-8859-1", "UTF-8"), $this->format["b"]);
+    }
+
+    private function slugController(string $slugController): string
+    {
+        // Tudo em minúsculo
+        $slugController = strtolower($slugController);
+        // Remove os -
+        $slugController = str_replace('-', ' ', $slugController);
+        // Primeira letra de cada palavra em maíusculo
+        $slugController = ucwords($slugController);
+        // Remove o espaço
+        $slugController = str_replace(' ', '', $slugController);
+
+        $this->urlSlugController = $slugController;
+
+        return $this->urlSlugController;
+    }
+
+    public function slugMetodo(string $slugMetodo): string
+    {
+        $slugMetodo = $this->slugController($slugMetodo);
+
+        // Primeira letra minúscula
+        $slugMetodo = lcfirst($slugMetodo);
+
+        $this->urlSlugMetodo = $slugMetodo;
+        
+        return $this->urlSlugMetodo;
     }
 
     public function loadPage()
